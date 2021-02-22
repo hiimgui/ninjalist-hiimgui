7da4b3899e6b19b6691df2fdf07ab8bf6be41298
Guilherme Kageyama <guilhermekageyama@MacBook-Air.local>
2021-02-22 11:39:49 -0300

    initial

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,15 +1,30 @@
-import Head from 'next/head';
+import styles from '../../styles/ninjas.module.css'
+import Link from 'next/link'
 
-const Ninjas = () => {
-    return (
-        <>
-        <Head>
-        <title>Ninja List </title>
-        <meta name="keywords" content="ninjas"/>
-      </Head> 
-        <div></div>
-        </>
-     );
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+
+  return (
+    <>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </Link>
+      ))}
+    </>
+  );
 }
  
 export default Ninjas;
+ 
